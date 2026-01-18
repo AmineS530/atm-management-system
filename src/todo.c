28dac4396c0d70e03f250cc932d4bd04b0fa45df
Amine Sadik <idekwiam18@gmail.com>
2026-01-18 01:51:01 +0100

    updated todo

--- src/todo.c
+++
@@ -1,8 +0,0 @@
-#include "atm_sys.h"
-// account types
-// place holder : https://stackoverflow.com/questions/5309859/how-to-define-an-array-of-functions-in-c
-
-//     printf("\nEnter today's date(mm/dd/yyyy):");
-//     scanf("%d/%d/%d", &r.deposit.month, &r.deposit.day, &r.deposit.year);
-
-static user_status users[MAX_USERS];
